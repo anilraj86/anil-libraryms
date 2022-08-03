a93c1282f36acf4c842a14fc5f23ee1094b29109
anilraj86 <102684771+anilraj86@users.noreply.github.com>
2022-08-03 12:22:49 +0400

    test 5

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.get('/api/booklist',function(req,res){
     //             .then(function(books){
     //                 res.send(books);
     //             });
-    res.send("Hello World");
+    return res.send("Hello World");
 });
 
 app.put('/api/updatebook',function(req,res){
